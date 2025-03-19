f17d546abdba633e642b748a380eeae57ad05bd7
Vitaog <vitaoog@gmail.com>
2025-03-19 19:42:46 -0300

    feat: Adição de conexão MongoDB ao projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ChamadoModule } from './chamado/chamado.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UsuarioModule, ChamadoModule],
+  imports: [PrismaModule, UsuarioModule, ChamadoModule, MongooseModule.forRoot(process.env.MONGO_URI || ""), ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
