5f5e1f0f6e26212f97ccb0fccc7f483bf5abed78
igorpereira28 <igorpereir28@hotmail.com>
2025-05-21 22:12:36 -0300

    feat: Usuário com login configurado, falta teste e adaptação para role

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,17 +1,30 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Delete, Param } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
+import { Roles } from '../auth/roles.decorator';
+import { RolesGuard } from '../auth/roles.guard';
 
 @Controller('usuarios')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
   @Post()
-  criarUsuario(@Body() body: { email: string; senha: string; papel: string }) {
-    return this.usuarioService.criarUsuario(body.email, body.senha, body.papel);
+  @UseGuards(RolesGuard)
+  @Roles('admin')
+  criarUsuario(@Body() body: { email: string; senha: string; papel: string, nome: string }) {
+    return this.usuarioService.criarUsuario(body.email, body.senha, body.papel, body.nome);
   }
 
   @Get()
+  @UseGuards(RolesGuard)
+  @Roles('admin')
   listarUsuarios() {
     return this.usuarioService.listarUsuarios();
+  }
+
+  @Delete(':id/desativar')
+  @UseGuards(RolesGuard)
+  @Roles('admin')
+  desativarUsuario(@Param('id') id: string) {
+    return this.usuarioService.desativarUsuario(id);
   }
 }
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,18 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Usuario } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsuarioService {
   constructor(private prisma: PrismaService) {}
 
-  async criarUsuario(email: string, senha: string, papel: string): Promise<Usuario> {
-    return this.prisma.usuario.create({
-      data: { email, senha, papel },
-    });
-  }
+  async criarUsuario(email: string, senha: string, papel: string, nome: string) {
+  const saltRounds = 10;
+  const senhaHash = await bcrypt.hash(senha, saltRounds);
+  
+  console.log(`Criando usuário ${email} com hash:`, senhaHash);
+  
+  return this.prisma.usuario.create({
+    data: {
+      email,
+      senha: senhaHash,
+      papel,
+      nome,
+      status: true
+    }
+  });
+}
 
   async listarUsuarios(): Promise<Usuario[]> {
     return this.prisma.usuario.findMany();
+  }
+
+  async desativarUsuario(id: string): Promise<Usuario> {
+    return this.prisma.usuario.update({
+      where: { id },
+      data: { status: false },
+    });
+  }
+
+  async encontrarPorEmail(email: string): Promise<Usuario | null> {
+    return this.prisma.usuario.findUnique({ where: { email } });
   }
 }
